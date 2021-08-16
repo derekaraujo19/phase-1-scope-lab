b89c7ab3e5b1e6e4afafba773dc04ff78b898b5a
Derek Araujo <derekaraujo19@gmail.com>
2021-08-16 14:46:43 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = 'BOB';
+}
+
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Sarah';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Dan';
+}
